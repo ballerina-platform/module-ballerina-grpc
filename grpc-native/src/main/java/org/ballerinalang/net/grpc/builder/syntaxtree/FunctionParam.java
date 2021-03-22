48aadbd6a62f8127211163e6a849965d0fa12887
MadhukaHarith <harithmadhu@gmail.com>
2021-03-22 23:19:42 +0530

    Add classes representing to syntax tree nodes

---
+++ grpc-native/src/main/java/org/ballerinalang/net/grpc/builder/syntaxtree/FunctionParam.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.ballerinalang.net.grpc.builder.syntaxtree;
+
+import io.ballerina.compiler.syntax.tree.AbstractNodeFactory;
+import io.ballerina.compiler.syntax.tree.AnnotationNode;
+import io.ballerina.compiler.syntax.tree.IncludedRecordParameterNode;
+import io.ballerina.compiler.syntax.tree.Node;
+import io.ballerina.compiler.syntax.tree.NodeFactory;
+import io.ballerina.compiler.syntax.tree.NodeList;
+import io.ballerina.compiler.syntax.tree.RequiredParameterNode;
+import org.ballerinalang.net.grpc.builder.constants.SyntaxTreeConstants;
+
+public class FunctionParam {
+
+    public static RequiredParameterNode getRequiredParamNode(Node typeName, String name) {
+        NodeList<AnnotationNode> annotations = NodeFactory.createEmptyNodeList();
+        return NodeFactory.createRequiredParameterNode(
+                annotations,
+                typeName,
+                AbstractNodeFactory.createIdentifierToken(name)
+        );
+    }
+
+    public static IncludedRecordParameterNode getIncludedRecordParamNode(Node typeName, String name) {
+        NodeList<AnnotationNode> annotations = NodeFactory.createEmptyNodeList();
+        return NodeFactory.createIncludedRecordParameterNode(
+                annotations,
+                SyntaxTreeConstants.SYNTAX_TREE_ASTERISK,
+                typeName,
+                AbstractNodeFactory.createIdentifierToken(name)
+        );
+    }
+}
