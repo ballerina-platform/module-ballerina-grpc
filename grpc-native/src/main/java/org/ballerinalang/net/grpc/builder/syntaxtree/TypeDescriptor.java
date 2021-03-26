b2b6ad1f7cb696d8aecbbc2a61704269c7edcf5c
MadhukaHarith <harithmadhu@gmail.com>
2021-03-26 15:49:20 +0530

    Add TypeDescriptor functions

--- grpc-native/src/main/java/org/ballerinalang/net/grpc/builder/syntaxtree/TypeDescriptor.java
+++ grpc-native/src/main/java/org/ballerinalang/net/grpc/builder/syntaxtree/TypeDescriptor.java
@@ -19,18 +19,24 @@
 package org.ballerinalang.net.grpc.builder.syntaxtree;
 
 import io.ballerina.compiler.syntax.tree.AbstractNodeFactory;
+import io.ballerina.compiler.syntax.tree.ArrayTypeDescriptorNode;
 import io.ballerina.compiler.syntax.tree.BuiltinSimpleNameReferenceNode;
+import io.ballerina.compiler.syntax.tree.Node;
 import io.ballerina.compiler.syntax.tree.NodeFactory;
+import io.ballerina.compiler.syntax.tree.NodeList;
+import io.ballerina.compiler.syntax.tree.ObjectFieldNode;
 import io.ballerina.compiler.syntax.tree.OptionalTypeDescriptorNode;
+import io.ballerina.compiler.syntax.tree.ParameterizedTypeDescriptorNode;
 import io.ballerina.compiler.syntax.tree.QualifiedNameReferenceNode;
 import io.ballerina.compiler.syntax.tree.SimpleNameReferenceNode;
 import io.ballerina.compiler.syntax.tree.SyntaxKind;
 import io.ballerina.compiler.syntax.tree.TypeDescriptorNode;
 import io.ballerina.compiler.syntax.tree.TypeParameterNode;
+import io.ballerina.compiler.syntax.tree.TypeReferenceNode;
 import io.ballerina.compiler.syntax.tree.UnionTypeDescriptorNode;
 import org.ballerinalang.net.grpc.builder.constants.SyntaxTreeConstants;
 
-public class TypeName {
+public class TypeDescriptor {
 
     public static QualifiedNameReferenceNode getQualifiedNameReferenceNode(String modulePrefix, String identifier) {
         return NodeFactory.createQualifiedNameReferenceNode(
@@ -49,7 +55,12 @@ public class TypeName {
     }
 
     public static BuiltinSimpleNameReferenceNode getBuiltinSimpleNameReferenceNode(String name) {
-        return NodeFactory.createBuiltinSimpleNameReferenceNode(SyntaxKind.STREAM_TYPE_DESC, AbstractNodeFactory.createIdentifierToken(name + " "));
+        switch (name) {
+            case "int" :
+                return NodeFactory.createBuiltinSimpleNameReferenceNode(SyntaxKind.INT_TYPE_DESC, AbstractNodeFactory.createIdentifierToken(name + " "));
+            default:
+                return NodeFactory.createBuiltinSimpleNameReferenceNode(SyntaxKind.STRING_TYPE_DESC, AbstractNodeFactory.createIdentifierToken(name + " "));
+        }
     }
 
     public static SimpleNameReferenceNode getSimpleNameReferenceNode(String name) {
@@ -58,5 +69,46 @@ public class TypeName {
 
     public static TypeParameterNode getTypeParameterNode(TypeDescriptorNode typeNode) {
         return NodeFactory.createTypeParameterNode(SyntaxTreeConstants.SYNTAX_TREE_IT, typeNode, SyntaxTreeConstants.SYNTAX_TREE_GT);
+    }
+
+    public static ParameterizedTypeDescriptorNode getParameterizedTypeDescriptorNode(String type, TypeDescriptorNode descriptorNode) {
+        return NodeFactory.createParameterizedTypeDescriptorNode(
+                AbstractNodeFactory.createIdentifierToken(type),
+                getTypeParameterNode(descriptorNode)
+        );
+    }
+
+    public static ArrayTypeDescriptorNode getArrayTypeDescriptorNode(String type) {
+        return NodeFactory.createArrayTypeDescriptorNode(
+                getBuiltinSimpleNameReferenceNode(type),
+                SyntaxTreeConstants.SYNTAX_TREE_OPEN_BRACKET,
+                null,
+                SyntaxTreeConstants.SYNTAX_TREE_CLOSE_BRACKET
+        );
+    }
+
+    public static TypeReferenceNode getTypeReferenceNode(Node typeName) {
+        return NodeFactory.createTypeReferenceNode(
+                SyntaxTreeConstants.SYNTAX_TREE_ASTERISK,
+                typeName,
+                SyntaxTreeConstants.SYNTAX_TREE_SEMICOLON
+        );
+    }
+
+    public static ObjectFieldNode getObjectFieldNode(String visibility, String[] qualifiers, Node typeName, String fieldName) {
+        NodeList qualifierList = NodeFactory.createEmptyNodeList();
+        for (String qualifier : qualifiers) {
+            qualifierList = qualifierList.add(AbstractNodeFactory.createIdentifierToken(qualifier + " "));
+        }
+        return NodeFactory.createObjectFieldNode(
+                null,
+                AbstractNodeFactory.createIdentifierToken("\n" + visibility + " "),
+                qualifierList,
+                typeName,
+                AbstractNodeFactory.createIdentifierToken(fieldName),
+                null,
+                null,
+                SyntaxTreeConstants.SYNTAX_TREE_SEMICOLON
+        );
     }
 }
