c1fb61a56bf55793b264ea02814dedaf4f5da005
MadhukaHarith <harithmadhu@gmail.com>
2021-04-05 15:44:10 +0530

    Update functions to handle different Message field types

--- grpc-native/src/main/java/org/ballerinalang/net/grpc/builder/BallerinaFileBuilder.java
+++ grpc-native/src/main/java/org/ballerinalang/net/grpc/builder/BallerinaFileBuilder.java
@@ -112,7 +112,7 @@ public class BallerinaFileBuilder {
     }
 
     public static SyntaxTree syntaxTreeDemo() {
-        Path sourceFilePath = Paths.get(resourceDirectory.toString(), "Chat_sample_service.bal");
+        Path sourceFilePath = Paths.get(resourceDirectory.toString(), "oneof_field_service_pb.bal");
         String content = null;
         try {
             content = getSourceText(sourceFilePath);
