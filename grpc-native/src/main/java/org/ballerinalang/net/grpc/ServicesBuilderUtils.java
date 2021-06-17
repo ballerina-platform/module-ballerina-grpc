c9c8933cd5851c8ebad5045a9f0c0c7e2939753d
BuddhiWathsala <b.wathsala.bw@gmail.com>
2021-06-17 20:16:48 +0530

    Fix service resolution for streaming cases with context
    When there is a context, the previous logic does not check for the stream type and it just return a record.
    Thus, it leads to a incorrect service resolution. Here, it added the check.

--- grpc-native/src/main/java/org/ballerinalang/net/grpc/ServicesBuilderUtils.java
+++ grpc-native/src/main/java/org/ballerinalang/net/grpc/ServicesBuilderUtils.java
@@ -331,7 +331,12 @@ public class ServicesBuilderUtils {
             return ((StreamType) inputType).getConstrainedType();
         } else if (inputType instanceof RecordType && inputType.getName().startsWith("Context") &&
                 ((RecordType) inputType).getFields().size() == 2) {
-            return ((RecordType) inputType).getFields().get("content").getFieldType();
+            Type contentType = ((RecordType) inputType).getFields().get("content").getFieldType();
+
+            if (contentType instanceof StreamType) {
+                return ((StreamType) contentType).getConstrainedType();
+            }
+            return contentType;
         } else {
             return inputType;
         }
