a3a5cc244c27eccfb9abe494f573e3f9e11bd936
dilanSachi <dilansachinthanos@gmail.com>
2023-03-30 13:23:33 +0530

    Add invalid return type

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/grpc/plugin/CompilerPluginTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/grpc/plugin/CompilerPluginTest.java
@@ -315,8 +315,8 @@ public class CompilerPluginTest {
     @Test
     public void testCompilerPluginServiceWithRecordTypeCaller() {
 
-        String errMsg = "ERROR [helloworld_service.bal:(17:5,19:6)] when there are two parameters to a remote function," +
-                " the first one must be a caller type";
+        String errMsg = "ERROR [helloworld_service.bal:(17:5,19:6)] when there are two parameters to a " +
+                "remote function, the first one must be a caller type";
         Package currentPackage = loadPackage("package_21");
         PackageCompilation compilation = currentPackage.getCompilation();
         DiagnosticResult diagnosticResult = compilation.diagnosticResult();
@@ -331,8 +331,8 @@ public class CompilerPluginTest {
     @Test
     public void testCompilerPluginServiceWithInvalidParameterCount() {
 
-        String errMsg = "ERROR [helloworld_service.bal:(17:5,19:6)] when there are two parameters to a remote function," +
-                " the first one must be a caller type";
+        String errMsg = "ERROR [helloworld_service.bal:(17:5,19:6)] when there are two parameters to a " +
+                "remote function, the first one must be a caller type";
         Package currentPackage = loadPackage("package_22");
         PackageCompilation compilation = currentPackage.getCompilation();
         DiagnosticResult diagnosticResult = compilation.diagnosticResult();
@@ -340,6 +340,22 @@ public class CompilerPluginTest {
         Assert.assertEquals(diagnosticResult.errors().size(), 1);
         Assert.assertEquals(diagnostic.diagnosticInfo().code(),
                 GrpcCompilerPluginConstants.CompilationErrors.MAX_PARAM_COUNT.getErrorCode());
+        Assert.assertTrue(diagnosticResult.errors().stream().anyMatch(
+                d -> errMsg.equals(d.toString())));
+    }
+
+    @Test
+    public void testCompilerPluginServiceWithInvalidReturnType() {
+
+        String errMsg = "ERROR [helloworld_service.bal:(8:5,10:6)] only `error?` return type is " +
+                "allowed with the caller";
+        Package currentPackage = loadPackage("package_23");
+        PackageCompilation compilation = currentPackage.getCompilation();
+        DiagnosticResult diagnosticResult = compilation.diagnosticResult();
+        Diagnostic diagnostic = (Diagnostic) diagnosticResult.errors().toArray()[0];
+        Assert.assertEquals(diagnosticResult.errors().size(), 1);
+        Assert.assertEquals(diagnostic.diagnosticInfo().code(),
+                GrpcCompilerPluginConstants.CompilationErrors.RETURN_WITH_CALLER.getErrorCode());
         Assert.assertTrue(diagnosticResult.errors().stream().anyMatch(
                 d -> errMsg.equals(d.toString())));
     }
---
+++ compiler-plugin-tests/src/test/resources/test-src/package_23/helloworld_service.bal
@@ -0,0 +1,12 @@
+import ballerina/grpc;
+
+listener grpc:Listener ep = new (9090);
+
+@grpc:Descriptor {value: HELLOWORLDSTRING_DESC}
+service "helloWorld" on ep {
+
+    remote function hello(HelloWorldStringCaller caller, stream<string, grpc:Error?> clientStream) returns int {
+        return 1;
+    }
+}
+
