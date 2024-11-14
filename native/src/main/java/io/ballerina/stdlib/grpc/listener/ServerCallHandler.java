29d54201406adc0a2ff216675c15c28f16079447
warunalakshitha <warudanu@gmail.com>
2024-11-14 19:24:35 +0530

    Fix checkstyle errors

--- native/src/main/java/io/ballerina/stdlib/grpc/listener/ServerCallHandler.java
+++ native/src/main/java/io/ballerina/stdlib/grpc/listener/ServerCallHandler.java
@@ -18,7 +18,6 @@
 package io.ballerina.stdlib.grpc.listener;
 
 import com.google.protobuf.Descriptors;
-import io.ballerina.runtime.api.Environment;
 import io.ballerina.runtime.api.concurrent.StrandMetadata;
 import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.types.ArrayType;
