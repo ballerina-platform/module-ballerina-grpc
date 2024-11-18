1e520b63787c2513e328a3f9f6e74b2a824365b8
warunalakshitha <warudanu@gmail.com>
2024-11-18 14:52:44 +0530

    Use main branch for github actions

--- native/src/main/java/io/ballerina/stdlib/grpc/GrpcUtil.java
+++ native/src/main/java/io/ballerina/stdlib/grpc/GrpcUtil.java
@@ -482,6 +482,9 @@ public class GrpcUtil {
             return balFuture.get();
         } catch (BError error) {
             throw error;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw ErrorCreator.createError(e);
         } catch (Throwable throwable) {
             throw ErrorCreator.createError(throwable);
         }
